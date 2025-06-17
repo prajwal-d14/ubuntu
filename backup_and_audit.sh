c56f32acc6b7d9cb988d31a102493f4a3e9f21f2
Ubuntu <ubuntu@ip-172-31-7-95.ap-south-1.compute.internal>
2025-06-17 12:28:26 +0000

    edited script

--- backup_and_audit.sh
+++ backup_and_audit.sh
@@ -1,38 +1,37 @@
 #!/bin/bash
 
 Today=$(date +%Y%m%d_%H%M%S)
-file="/home/ec2-user/repo-list.txt"
+file="/home/ubuntu/repo-list.txt"
 
 while IFS= read -r repo; do
-    if [ -z "$repo" ]; then
-        continue
-		
+	if [ -z "$repo" ]; then
+		continue
+
 	fi
-	
-	reponame=$(basename "$repo" .git)
-	
-	if [ -d "$reponame" ]; then
-	
+
+	reponame=$(echo "$repo" | awk -F "/" '{print $NF}' | awk -F "." '{print $1}')
+)
+
+if [ -d "$reponame" ]; then
+
 	cd $reponame
-	
-    git pull $repo
-	
+
+	git pull $repo
+
 	sleep 10
-	
-	sudo tar -czf /home/ec2-user/git-backup-audit/${reponame}-$Today.tar.gz -C /home/ec2-user $reponame
+
+	sudo tar -czf /home/ubuntu/git-backup-audit/${reponame}-$Today.tar.gz -C /home/ubuntu $reponame
 
 	sleep 5
-	
-	git log --since=1.day >> /home/ec2-user/$reponame-audit-$Today.txt
-	
-	cd ..
-	
-	sudo mv	$reponame-audit-$Today.txt /home/ec2-user/git-backup-audit/
-	
-	else
-	
-		git clone $repo
-		
-	fi
 
-	done < "$file"
+	sudo git log --since=1.day >> /home/ubuntu/$reponame-audit-$Today.txt
+
+	cat $reponame-audit-$Today.txt 
+
+else
+
+	git clone $repo
+
+fi
+
+done < "$file"
